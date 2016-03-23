0dbfc98a56e03e1e4990067f84ed4dea62c7fc3d
Mateusz Kulus <35586@student.ue.poznan.pl>
2016-03-23 12:57:37 +0100

    Dodalem funkcje get info

--- calc.py
+++ calc.py
@@ -3,6 +3,10 @@ def dodawanie():
 	a = int(input("Podaj a"))
 	b = int(input("podaj B"))
 	print(a+b)
+
+def get_info():
+	return "To jest program kalkulator stworzony przez Mateusza"
 	
+print(get_info())
 dodawanie()
 input()
