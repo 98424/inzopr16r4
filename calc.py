0e623c1515d114818c9d245114088c020710d78b
Mateusz Kulus <35586@student.ue.poznan.pl>
2016-03-23 13:09:47 +0100

    Poprawiono funkcje dodawania

--- calc.py
+++ calc.py
@@ -1,9 +1,11 @@
 print("Hello World")
 def dodawanie(a, b):
 	return a+b
-
-a = int(input("Podaj 1 liczbe"))
-b = int(input("Podaj 2 liczbe"))
-
+try:
+	a = int(input("Podaj 1 liczbe"))
+	b = int(input("Podaj 2 liczbe"))
+	print(dodawanie(a,b))
+except ValueError as ve:
+	print("Wprowadzono bledne dane, koniec..")
 print(dodawanie(a,b))
 input()
